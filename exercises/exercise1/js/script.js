ccfe484bc05200d9ef5f8374ab4d9564fb819011
Liu WenYue <LiuWY0912@gmail.com>
2018-09-17 18:26:59 -0400

    Ex1: Added another image that follows the mouse at a different speed

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -7,28 +7,36 @@
 // One moves toward the mouse cursor at a different speed.
 
 
-// The image of ghost that moves from the left to the right of the screen
+// The image of ghost that moves from the left to the right of the screen.
 var ghostImage;
-// The current position of the ghost image
+// The current position of the ghost image.
 var ghostImageX;
 var ghostImageY;
 
 
-// The image of a spider
+// The image of a spider.
 var spiderImage;
-// The current position of the spider
+// The current position of the spider.
 var spiderImageX;
 var spiderImageY;
+
+
+// The image of a pumpkin with hat.
+var pumpkinImage;
+// The current position of the pumpkin with a hat.
+var pumpkinImageX;
+var pumpkinImageY;
 
 
 
 // preload()
 //
-// Load the three images we're using before the program starts
+// Load the three images we're using before the program starts.
 
 function preload() {
   ghostImage = loadImage("assets/images/ghost.png");
   spiderImage = loadImage("assets/images/spider.png");
+  pumpkinImage = loadImage("assets/images/pumpkin.png");
 }
 
 
@@ -37,61 +45,60 @@ function preload() {
 // Set up the canvas, position the images, set the image mode.
 
 function setup() {
-  // Create our canvas
+  // Create our canvas.
   createCanvas(640,640);
 
-  // Start the ghost image at the left side of the canvas
+  // Start the ghost image off screen at the left side of the canvas.
   ghostImageX = 0 - ghostImage.width/4;
   ghostImageY = height/2;
 
-  // Start the clown image at the centre of the canvas
-  // clownImageX = width/2;
-  // clownImageY = height/2;
+  // Start the pumpkinImage in the centre of the canvas.
+  pumpkinImageX = width/2;
+  pumpkinImageY = height/2;
 
-  // Start the felt image perfectly off screen above the canvas
-  // feltTextureImageX = width/2;
-  // feltTextureImageY = 0 - feltTextureImage.height/2;
-
-  // We'll use imageMode CENTER for this script
+  // We'll use imageMode CENTER for this script.
   imageMode(CENTER);
 }
 
 
 // draw()
 //
-// Moves the ghost image from the left to the right of the screen
-// Moves the felt image linearly
-// Moves the clown face toward the current mouse location
+// Moves the ghost image from the left to the right of the screen.
+// Have the spider image follows the mouse's movement.
+// Have the pumpkin image follows the mouse cursor at a different speed.
 
 function draw() {
-  // Move the ghost image from left to the right by increasing its x position
+  // Move the ghost image from left to the right by increasing its x position.
   ghostImageX += 1;
 
-  // Display the ghost image in 1.5 times
-  image(ghostImage,ghostImageX,ghostImageY,ghostImage.width * 0.5,ghostImage.height * 0.5);
+  // Display the ghost image.
+  image(ghostImage,ghostImageX,ghostImageY,ghostImage.width * 0.65,ghostImage.height * 0.65);
 
-  // Start the spider image at the position of the cursor and follows its movement
+
+  // Start the spider image at the position of the mouse and follows its movement.
   spiderImageX = mouseX;
   spiderImageY = mouseY;
 
-  // Display the spider image
-  image(spiderImage,spiderImageX,spiderImageY,spiderImage.width * 0.5,spiderImage.height * 0.5);
+  // Display the spider image.
+  image(spiderImage,spiderImageX,spiderImageY,spiderImage.width * 0.45,spiderImage.height * 0.45);
 
-  // Move the felt image down by increasing its y position
-  // feltTextureImageY += 1;
 
-  // Display the felt image
-  // image(feltTextureImage,feltTextureImageX,feltTextureImageY);
+  // Updates the location of pumpkin image using linear interpolation based on
+  // the distance between the pumpkin image and the mouse.
 
-  // Move the clown by moving it 1/10th of its current distance from the mouse
+  // Calculate the distance between the pumpkinImage and the mouse.
+  var d = dist(pumpkinImageX,pumpkinImageY,mouseX,mouseY);
 
-  // Calculate the distance in X and in Y
-  // var xDistance = mouseX - clownImageX;
-  // var yDistance = mouseY - clownImageY;
-  // Add 1/10th of the x and y distance to the clown's current (x,y) location
-  // clownImageX = clownImageX + xDistance/10;
-  // clownImageY = clownImageY + yDistance/10;
+  // Use linear interpolation to update the location of the pumpkinImage based
+  // on the distance.
 
-  // Display the clown image
-  // image(clownImage,clownImageX,clownImageY);
+  // When the distance is big, 1/d will be very small, so the pumpkinImage will
+  // move a small fraction of the remaining distance.
+  // When the distance is small, 1/d will approach 1 and the pumpkinImage will
+  // move a large fraction of the remaining distance.
+  pumpkinImageX = lerp(pumpkinImageX,mouseX,1/d);
+  pumpkinImageY = lerp(pumpkinImageY,mouseY,1/d);
+
+  // Display the pumpkin image.
+  image(pumpkinImage,pumpkinImageX,pumpkinImageY,pumpkinImage.width * 0.5,pumpkinImage.height * 0.5);
 }
