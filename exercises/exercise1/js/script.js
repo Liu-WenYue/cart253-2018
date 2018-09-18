5aaecada4adb6f58647dbd2b81bfc196cbd2753f
Liu WenYue <LiuWY0912@gmail.com>
2018-09-17 21:02:57 -0400

    Ex1: Added Background Image and Final Checking

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -28,6 +28,10 @@ var pumpkinImageX;
 var pumpkinImageY;
 
 
+// The image of the background in purple.
+var purpleBackdropImage;
+
+
 
 // preload()
 //
@@ -37,7 +41,9 @@ function preload() {
   ghostImage = loadImage("assets/images/ghost.png");
   spiderImage = loadImage("assets/images/spider.png");
   pumpkinImage = loadImage("assets/images/pumpkin.png");
+  purpleBackdropImage = loadImage("assets/images/purple_backdrop.png");
 }
+
 
 
 // setup()
@@ -45,7 +51,7 @@ function preload() {
 // Set up the canvas, position the images, set the image mode.
 
 function setup() {
-  // Create our canvas.
+  // Create the canvas.
   createCanvas(640,640);
 
   // Start the ghost image off screen at the left side of the canvas.
@@ -56,9 +62,13 @@ function setup() {
   pumpkinImageX = width/2;
   pumpkinImageY = height/2;
 
+  // Display the purple background image.
+  image(purpleBackdropImage, 0, 0, width, height);
+
   // We'll use imageMode CENTER for this script.
   imageMode(CENTER);
 }
+
 
 
 // draw()
@@ -72,7 +82,7 @@ function draw() {
   ghostImageX += 1;
 
   // Display the ghost image.
-  image(ghostImage,ghostImageX,ghostImageY,ghostImage.width * 0.65,ghostImage.height * 0.65);
+  image(ghostImage, ghostImageX, ghostImageY, ghostImage.width * 0.5, ghostImage.height * 0.5);
 
 
   // Start the spider image at the position of the mouse and follows its movement.
@@ -80,14 +90,14 @@ function draw() {
   spiderImageY = mouseY;
 
   // Display the spider image.
-  image(spiderImage,spiderImageX,spiderImageY,spiderImage.width * 0.45,spiderImage.height * 0.45);
+  image(spiderImage, spiderImageX, spiderImageY, spiderImage.width * 0.45, spiderImage.height * 0.45);
 
 
   // Updates the location of pumpkin image using linear interpolation based on
   // the distance between the pumpkin image and the mouse.
 
   // Calculate the distance between the pumpkinImage and the mouse.
-  var d = dist(pumpkinImageX,pumpkinImageY,mouseX,mouseY);
+  var d = dist(pumpkinImageX, pumpkinImageY, mouseX, mouseY);
 
   // Use linear interpolation to update the location of the pumpkinImage based
   // on the distance.
@@ -96,9 +106,9 @@ function draw() {
   // move a small fraction of the remaining distance.
   // When the distance is small, 1/d will approach 1 and the pumpkinImage will
   // move a large fraction of the remaining distance.
-  pumpkinImageX = lerp(pumpkinImageX,mouseX,1/d);
-  pumpkinImageY = lerp(pumpkinImageY,mouseY,1/d);
+  pumpkinImageX = lerp(pumpkinImageX, mouseX, 1/d);
+  pumpkinImageY = lerp(pumpkinImageY, mouseY, 1/d);
 
   // Display the pumpkin image.
-  image(pumpkinImage,pumpkinImageX,pumpkinImageY,pumpkinImage.width * 0.5,pumpkinImage.height * 0.5);
+  image(pumpkinImage, pumpkinImageX, pumpkinImageY, pumpkinImage.width * 0.5, pumpkinImage.height * 0.5);
 }
