ad03b774ac48314f0272f1589a855fe6524f68b1
Liu WenYue <LiuWY0912@gmail.com>
2018-09-25 13:34:14 -0400

    Ex2: Added new if-statements for the conditions of Reward

--- exercises/exercise2/js/script.js
+++ exercises/exercise2/js/script.js
@@ -8,37 +8,54 @@ Liu WenYue
 
 *********************************************************/
 
-// The image of the avatar, octopus.
+// The image of the avatar octopus.
 var avatarImage;
 
-// The position and size of our avatar, octopus.
+// The position and size of our avatar octopus.
 var avatarX;
 var avatarY;
 var avatarSize = 85;
 
-// The speed and velocity of our avatar, octopus.
+// The speed and velocity of our avatar octopus.
 var avatarSpeed = 10;
 var avatarVX = 0;
 var avatarVY = 0;
 
-// The image of the enemy, water bubble.
+// The image of the enemy water bubble.
 var enemyImage;
 
-// The position and size of the enemy, water bubble
+// The position and size of the enemy water bubble.
 var enemyX;
 var enemyY;
 var enemySize = 50;
-// How much bigger the enemy circle gets with each successful dodge.
+// How much bigger the enemy gets with each successful dodge.
 var enemySizeIncrease = 5;
 
-// The speed and velocity of our enemy circle.
+// The speed and velocity of our enemy water bubble.
 var enemySpeed = 5;
 var enemyVX = 5;
-// How much bigger the enemy circle gets with each successful dodge.
+// How much faster the enemy gets with each successful dodge.
 var enemySpeedIncrease = 0.5;
+
+// The image of reward, starfish.
+var rewardImage;
+
+// The position and size of the reward starfish.
+var rewardX;
+var rewardY;
+var rewardSize = 40;
+
+// The speed, velocity and acceleration of the reward starfish.
+var rewardSpeed = 5;
+var rewardVX = 2;
+// How much faster the enemy gets with each successful dodge.
+var rewardSpeedIncrease = 0.1;
 
 // How many dodges the player has made.
 var dodges = 0;
+
+// How many more dodges awarded while the avatar goes near the starfish.
+var dodgesIncrease = 1;
 
 // My GearedSlab-Light Font file.
 var myFont;
@@ -55,6 +72,7 @@ function preload() {
   avatarImage = loadImage("assets/images/avatar.png")
   enemyImage = loadImage("assets/images/enemy.png")
   backgroundImage = loadImage("assets/images/background.png")
+  rewardImage = loadImage("assets/images/reward.png")
 }
 
 // setup()
@@ -71,6 +89,10 @@ function setup() {
   // Put the enemy to the left at a random y coordinate within the canvas.
   enemyX = 0;
   enemyY = random(0,height);
+
+  // Put the reward to the left at a random y coordinate within the canvas.
+  rewardX = 0;
+  rewardY = random(0,height);
 
   // We'll use imageMode CENTER for this script.
   imageMode(CENTER);
@@ -90,7 +112,7 @@ function draw() {
   textSize(40);
   textFont(myFont);
   textAlign(LEFT, TOP);
-  text( dodges + " dodges!", 12, 12);
+  text(dodges + " dodges!", 12, 12);
 
   // Default the avatar's velocity to 0 in case no key is pressed this frame.
   avatarVX = 0;
@@ -124,6 +146,11 @@ function draw() {
   enemyVX = enemySpeed;
   // Update the enemy's position based on its velocity.
   enemyX = enemyX + enemyVX;
+
+  // The reward always moves at rewardSpeed.
+  rewardVX = rewardSpeed;
+  // Update the enemy's position based on its velocity.
+  rewardX = rewardX + rewardVX;
 
   // Check if the enemy and avatar overlap - if they do the player loses.
   // We do this by checking if the distance between the centre of the enemy
@@ -171,6 +198,28 @@ function draw() {
     enemySize = enemySize + enemySizeIncrease;
   }
 
+  // Check if the reward has moved all the way across the screen.
+  if (rewardX > width) {
+    // Reset the reward's position to the left at a random height.
+    rewardX = 0;
+    rewardY = random(0,height);
+    // Increase the reward's speed to make the game harder.
+    rewardSpeed = rewardSpeed + rewardSpeedIncrease;
+    rewardVX = constrain(rewardVX, -rewardSpeed, rewardSpeed)
+  }
+
+  // Check if the reward and avatar overlap, if they do the player gets dodges
+  // while the avatar overlaps the reward. We do this by checking if the
+  // distance between the centre of the reward and the centre of the avatar
+  // is less that their combined radii.
+  if (dist(rewardX,rewardY,avatarX,avatarY) < rewardSize/2 + avatarSize/2) {
+    // Reset the dodge counter.
+    dodges += dodgesIncrease;
+  }
+  else {
+    dodges += 0;
+  }
+
   // Display the current number of successful in the console.
   console.log(dodges);
 
@@ -180,4 +229,6 @@ function draw() {
   // Display the enemy image.
   image(enemyImage, enemyX, enemyY, enemySize, enemySize);
 
+  // Display the reward image.
+  image(rewardImage, rewardX, rewardY, rewardSize, rewardSize);
 }
