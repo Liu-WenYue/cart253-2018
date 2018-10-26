65359575f17b6ea42cde8a61ae7d94263708300c
Liu WenYue <LiuWY0912@gmail.com>
2018-10-25 23:54:27 -0400

    Ex6: Syntax error, removed the extra opening parenthesis

--- exercises/exercise6/js/Ball.js
+++ exercises/exercise6/js/Ball.js
@@ -85,7 +85,14 @@ Ball.prototype.display = function () {
 //
 // Check if this ball overlaps the paddle passed as an argument
 // and if so reverse x velocity to bounce
-Ball.prototyp.handleCollision = function((paddle) {
+
+/////////////////////   NEW   /////////////////////
+
+// Syntax error, removed the extra opening parenthesis.
+Ball.prototyp.handleCollision = function(paddle) {
+
+/////////////////////   END NEW  /////////////////////
+
   // Check if the ball overlaps the paddle on x axis
   if (this.x + this.size > paddle.x && this.x < paddle.x + paddle.w) {
     // Check if the ball overlaps the paddle on y axis
