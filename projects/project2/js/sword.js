d272ef2a7517a0230e0b72f56c263771347a5af7
Liu WenYue <LiuWY0912@gmail.com>
2018-11-06 01:15:49 -0500

    P2: Added the sword constructor

--- projects/project2/js/sword.js
+++ projects/project2/js/sword.js
@@ -2,3 +2,19 @@
 //
 // A class that defines how a sword behaves, including how it reduces the
 // paddles ability.
+
+/////////////////////   NEW   /////////////////////
+
+// Sword constructor
+//
+// Sets the properties with the provided arguments
+function Sword(x,y,vx,vy,size,speed) {
+  this.x = x;
+  this.y = y;
+  this.vx = vx;
+  this.vy = vy;
+  this.size = size;
+  this.speed = speed;
+}
+
+/////////////////////   END NEW  /////////////////////
