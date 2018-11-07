336f1910acf1c4002e08c89cb79a85d01512f0ce
Liu WenYue <LiuWY0912@gmail.com>
2018-11-06 21:30:40 -0500

    P2: Created the basic properties for the mushroom

--- projects/project2/js/Mushroom.js
+++ projects/project2/js/Mushroom.js
@@ -2,3 +2,19 @@
 //
 // A class that defines how a mushroom behaves, including how it reduces the
 // ball ability.
+
+/////////////////////   NEW   /////////////////////
+
+// Mushroom constructor
+//
+// Sets the properties with the provided arguments
+function Mushroom(x,y,vx,vy,size,speed) {
+  this.x = x;
+  this.y = y;
+  this.vx = vx;
+  this.vy = vy;
+  this.size = size;
+  this.speed = speed;
+}
+
+/////////////////////   END NEW  /////////////////////
