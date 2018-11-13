4d5db5d3d1c5a754c9904be634765881217bc85c
Liu WenYue <LiuWY0912@gmail.com>
2018-11-13 11:34:52 -0500

    Ex7: Added Player constructor

--- exercises/exercise7/js/Player.js
+++ exercises/exercise7/js/Player.js
@@ -2,3 +2,16 @@
 //
 // A class that defines how a player behaves, including the ability
 // to specify the input keys to move it in all directions.
+
+// Player constructor
+//
+// Sets the properties with the provided arguments for Player.
+function Player(x,y,vx,vy,size,speed) {
+  this.x = x;
+  this.y = y;
+  this.displacementX = dx; // X Distance (left and right) moved by the player.
+  this.displacementY = dy; // Y Distance (up and down) moved by the player.
+  this.size = size;
+  this.startVX = vx; // The X starting position for player.
+  this.startVY = vy; // The Y starting position for player.
+}
