b9321abed98d2ca8f46b6d4a6644ee1380763525
Liu WenYue <LiuWY0912@gmail.com>
2018-11-13 16:28:57 -0500

    Ex7: Modified the player constructor and added player keyPressed function that handles input

--- exercises/exercise7/js/Player.js
+++ exercises/exercise7/js/Player.js
@@ -6,12 +6,44 @@
 // Player constructor
 //
 // Sets the properties with the provided arguments for Player.
-function Player(x,y,vx,vy,size,speed) {
+function Player(x,y,size,rightKey,leftKey,upKey,downKey) {
   this.x = x;
   this.y = y;
-  this.displacementX = dx; // X Distance (left and right) moved by the player.
-  this.displacementY = dy; // Y Distance (up and down) moved by the player.
+  this.displacementX = 0; // X Distance (left and right) moved by the player.
+  this.displacementY = 0; // Y Distance (up and down) moved by the player.
   this.size = size;
-  this.startVX = vx; // The X starting position for player.
-  this.startVY = vy; // The Y starting position for player.
+  this.startX = x; // The X starting position for player.
+  this.startY = y; // The Y starting position for player.
+  this.rightKey = rightKey; // The controls of the handle input.
+  this.leftKey = leftKey;
+  this.upKey = upKey;
+  this.downKey = downKey;
+}
+
+
+// keyPressed()
+//
+// Checks if the arrow keys are pressed.
+Player.prototype.keyPressed = function() {
+  // If the upkey is pressed, the player will move up by one unit of its
+  // Y displacement, which is the player's size.
+  if (keyCode === this.upKey) {
+    this.displacementY -= this.size;
+  }
+  // If the downkey is pressed, the player will move down by one unit of its
+  // Y displacement, which is the player's size.
+  if (keyCode === this.downKey) {
+    this.displacementY += this.size;
+  }
+
+  // If the leftkey is pressed, the player will move to the left by one
+  // unit of its Y displacement, which is the player's size.
+  if (keyCode === this.leftKey) {
+    this.displacementX -= this.size;
+  }
+  // If the rightkey is pressed, the player will move to the right by one
+  // unit of its Y displacement, which is the player's size.
+  if (keyCode === this.rightKey) {
+    this.displacementX += this.size;
+  }
 }
