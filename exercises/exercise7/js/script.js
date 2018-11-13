daf81f99e7b0603cf2dd5e86493127fc5e632050
Liu WenYue <LiuWY0912@gmail.com>
2018-11-12 21:01:41 -0500

    Ex7: Started exercise 7 with game descriptions

---
+++ exercises/exercise7/js/script.js
@@ -0,0 +1,64 @@
+/****************************************************************************
+
+Final Project Prototype
+Liu WenYue
+
+This game is a Push the Box kind of game, where the player not pushing any
+items but he has to reach at his partner before his partner disappears.
+For every step the player moved, there will be health lost (reduce opacity)
+for the partner. Player have to reach to his partner before the partner is
+totally disappeared.
+
+There will be 5 stages in this game. Each stage will be in different seasons
+(winter, spring, summer, autumn and winter) and the characters will change age
+for every stage. The first stage is a tutorial stage for the player to
+understand how to play the game. For the second, third and fourth, there will
+be obstacles on the way, for instance, “toys” for the second stage, “other
+female characters” for the third stage and “diseases triggers” for the fourth
+stage. If the player touched any of the obstacles, game overs and an option
+for restart the stage will appear. There will also be items on the way to stop
+the health lost of the partner for three moves, the items will be “candy”,
+“ring” and “flowers”.
+
+For the final stage, there will not be any obstacles, but the player and the
+partner will both loss health as the player moves.
+
+
+In this first prototype, I will be doing the first stage of the game. The
+main purpose of this first prototype is to get the art style, basic functions
+and background music of the game ready. As this is the first stage of the game,
+it’s more like a tutorial stage for the player to understand how to play the
+game, the player just have to go straight to reach his partner and there will
+not be any obstacles yet. During the game, there will be some text instructions,
+and there will also be starting page and game over page.
+
+The player will be able to move with the arrow keys.
+
+****************************************************************************/
+
+
+// preload()
+//
+// Description of preload
+
+function preload() {
+
+}
+
+
+// setup()
+//
+// Description of setup
+
+function setup() {
+
+}
+
+
+// draw()
+//
+// Description of draw()
+
+function draw() {
+
+}
