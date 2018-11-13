080af7505e1f3f6e0c73b43fcc089be3b432f476
Liu WenYue <LiuWY0912@gmail.com>
2018-11-13 16:52:56 -0500

    Ex7: Added the Target constructor

--- exercises/exercise7/js/Target.js
+++ exercises/exercise7/js/Target.js
@@ -2,3 +2,13 @@
 //
 // A class that defines how a target behaves, including losing health
 // for every move done by the player.
+
+// Target constructor
+//
+// Sets the properties with the provided arguments for Target.
+function Target(x,y,size,health) {
+  this.x = x;
+  this.y = y;
+  this.size = size;
+  this.health = health; // The health value for the target.
+}
