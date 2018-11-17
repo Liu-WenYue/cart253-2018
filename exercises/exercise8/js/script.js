f0903a9834c5aa261931ce948bdef9f918a2243d
Liu WenYue <LiuWY0912@gmail.com>
2018-11-16 23:37:57 -0500

    Ex7: Started exercise 7 with game descriptions

---
+++ exercises/exercise8/js/script.js
@@ -0,0 +1,65 @@
+/*****************
+
+Final Project Prototype
+Liu WenYue
+
+This game is a Push the Box kind of game, where the player not pushing any
+items but he has to reach at his partner before his partner disappears.
+For every step the player moved, there will be health lost (reduce opacity)
+for the partner. Player have to reach to his partner before the partner is
+totally disappeared.
+
+There will be 5 stages in this game. Each stage will be in different seasons
+(winter, spring, summer, autumn and winter) and the characters will change
+age for every stage. The first stage is a tutorial stage for the player to
+understand how to play the game. For the second, third and fourth, there will
+be obstacles on the way, for instance, “toys” for the second stage, “other
+female characters” for the third stage and “diseases triggers” for the fourth
+stage. If the player touched any of the obstacles, game overs and an option
+for restart the stage will appear. There will also be items on the way to
+stop the health lost of the partner for three moves, the items will be “candy”,
+“ring” and “flowers”. For the final stage, there will not be any obstacles,
+but the player and the partner will both loss health as the player moves.
+
+Moreover, for all the stages, I will be adding more props to make the scene
+more lively.
+
+In this second prototype, I will doing the second stage of the game. The main
+purpose of this second prototype is to make the additional items works in the
+game,  refine the style of the assets used in the game.  The second stage of
+the game will have the characters in their teenage time and the background is
+in spring. The map for this stage is definitely harder than the first stage.
+There will be “toys” on the way to prevent the player from reaching to his
+partner, if the player moved onto the “toy”, gameover will be triggered; there
+are also “candys”, if the player moved onto the “candy”, they can move three
+moved without losing their partner’s health.
+
+The player will be able to move with the arrow keys.
+
+******************/
+
+// preload()
+//
+// Description of preload
+
+function preload() {
+
+}
+
+
+// setup()
+//
+// Description of setup
+
+function setup() {
+
+}
+
+
+// draw()
+//
+// Description of draw()
+
+function draw() {
+
+}
