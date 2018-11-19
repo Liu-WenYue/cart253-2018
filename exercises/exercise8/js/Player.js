2424770faf3e17e25373d904aab977cf1a45dc48
Liu WenYue <LiuWY0912@gmail.com>
2018-11-19 14:44:04 -0500

    Ex8: Added keyPressed function for Player

--- exercises/exercise8/js/Player.js
+++ exercises/exercise8/js/Player.js
@@ -19,3 +19,31 @@ function Player(x,y,size,rightKey,leftKey,upKey,downKey) {
   this.upKey = upKey;
   this.downKey = downKey;
 }
+
+
+// keyPressed()
+//
+// Checks if the arrow keys are pressed.
+Player.prototype.keyPressed = function() {
+  // If the upkey is pressed, the player will move up by one unit of its
+  // Y displacement, which is the player's size.
+  if (keyCode === this.upKey) {
+    this.displacementY -= this.size;
+  }
+  // If the downkey is pressed, the player will move down by one unit of its
+  // Y displacement, which is the player's size.
+  if (keyCode === this.downKey) {
+    this.displacementY += this.size;
+  }
+
+  // If the leftkey is pressed, the player will move to the left by one
+  // unit of its Y displacement, which is the player's size.
+  if (keyCode === this.leftKey) {
+    this.displacementX -= this.size;
+  }
+  // If the rightkey is pressed, the player will move to the right by one
+  // unit of its Y displacement, which is the player's size.
+  if (keyCode === this.rightKey) {
+    this.displacementX += this.size;
+  }
+}
