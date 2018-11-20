0e5bab1e7b03395d65fab43f87cc6b4e8187bd43
Liu WenYue <LiuWY0912@gmail.com>
2018-11-19 21:43:25 -0500

    Ex8: Added Candy constructor

--- exercises/exercise8/js/Candy.js
+++ exercises/exercise8/js/Candy.js
@@ -2,3 +2,14 @@
 //
 // A class that defines how a candy behaves, including the ability
 // to stop the health lose of target for three rounds.
+
+// Candy constructor
+//
+// Sets the properties with the provided arguments for candy.
+function Candy(x,y,size,health) {
+  this.x = x;
+  this.y = y;
+  this.size = size;
+  this.health = health; // The health value for the candy.
+  this.startHealth = health; // The starting health for candy.
+}
