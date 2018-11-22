960a12a844745f4aeead38b8767c1669276040af
Liu WenYue <LiuWY0912@gmail.com>
2018-11-22 01:24:03 -0500

    P3: Started project 3

---
+++ projects/project3/js/script.js
@@ -0,0 +1,32 @@
+/****************************************************************************
+
+Seek
+by Liu WenYue
+
+Professor: Pippin Barr Ph.D
+
+****************************************************************************/
+
+
+// preload()
+//
+// Description of preload
+function preload() {
+
+}
+
+
+// setup()
+//
+// Description of setup
+function setup() {
+
+}
+
+
+// draw()
+//
+// Description of draw()
+function draw() {
+
+}
