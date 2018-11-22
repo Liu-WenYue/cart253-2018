c3f1f36a49010f0acab716040ad60d8e50471d8d
Liu WenYue <LiuWY0912@gmail.com>
2018-11-22 01:29:08 -0500

    P3: Added game description

--- projects/project3/js/script.js
+++ projects/project3/js/script.js
@@ -5,6 +5,12 @@ by Liu WenYue
 
 Professor: Pippin Barr Ph.D
 
+
+This game is about a character who is looking for his partner throughout his 
+lifetime.
+
+The player have the ability to move in all four directions using arrow keys.
+
 ****************************************************************************/
 
 
