18b942c0e2e01fc31177b9e38474c67c3c6b1f22
Liu WenYue <LiuWY0912@gmail.com>
2018-11-26 16:27:00 -0500

    P3: Added car constructor

--- projects/project3/js/Car.js
+++ projects/project3/js/Car.js
@@ -2,3 +2,15 @@
 //
 // A class that defines how a car behaves, including the ability to prevent
 // the player from reaching to their partner.
+
+// Car constructor
+//
+// Sets the properties with the provided arguments for Car.
+function Car(x,y,size,health) {
+  this.x = x;
+  this.y = y;
+  this.size = size;
+  this.health = health; // The health value for car.
+  this.startHealth = health; // The starting health for car.
+  this.harmful = true; // Sets the starting state of the car.
+}
