c9d9ea96bfb8854e22d1e58dcd9871d9640cfe7b
Liu WenYue <LiuWY0912@gmail.com>
2018-11-26 13:28:12 -0500

    P3: Added chakraPetch font

--- projects/project3/js/script.js
+++ projects/project3/js/script.js
@@ -6,7 +6,7 @@ by Liu WenYue
 Professor: Pippin Barr Ph.D
 
 
-This game is about a character who is looking for his partner throughout his 
+This game is about a character who is looking for his partner throughout his
 lifetime.
 
 The player have the ability to move in all four directions using arrow keys.
@@ -17,12 +17,14 @@ The player have the ability to move in all four directions using arrow keys.
 // Tracking the current state of the program (title screen to begin).
 var state = "TITLE";
 
+var chakraPetchFont; // The variable that stores the Chakra petch font.
+
 
 // preload()
 //
-// Description of preload
+// Preloads all the images and audios that will be used in this game.
 function preload() {
-
+  chakraPetchFont = loadFont("assets/font/chakrapetch_bold.ttf");
 }
 
 
@@ -74,5 +76,36 @@ function draw() {
     case "GAMEOVER":
     displayGameOver();
     break;
+  }
+}
+
+
+// displayTitle()
+//
+// Displays the starting page of the game, that includes game title,
+// game instructions and start game instruction.
+function displayTitle() {
+  push();//saves the current style settings.
+  noStroke(); // There will be no outline strokes on the elements.
+  fill(255,255,255,220); // Have a transparent white fill color.
+  rectMode(CENTER); // Set the rectMode to center.
+  rect(width/2,height/2, width, height); // Draws the rectangle that covers the screen.
+  pop();//restores these settings.
+
+  textAlign(CENTER,CENTER);
+  textSize(62);
+  fill(70);
+  stroke(70);
+  textFont(chakraPetchFont);
+  text("SEEK",width/2,4*height/9); // Display the game name.
+  textSize(34);
+  // Display the instructions for the controls and start game.
+  text("Click anywhere to Start\nUse arrow keys for Control",width/2,3*height/5);
+
+  // Check whether the mouse was pressed to start the game.
+  if (mouseIsPressed) {
+    // if mouse is pressed, change the state to "STAGE1" so the switch
+    // statement in draw() will display the stage 1 instead.
+    state = "STAGE1";
   }
 }
