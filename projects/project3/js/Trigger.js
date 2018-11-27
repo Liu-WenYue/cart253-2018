93ec47b3a63e317533c06a7700aa64a7ac52c8cc
Liu WenYue <LiuWY0912@gmail.com>
2018-11-26 21:39:28 -0500

    P3: Added trigger constructor

--- projects/project3/js/Trigger.js
+++ projects/project3/js/Trigger.js
@@ -2,3 +2,15 @@
 //
 // A class that defines how a car behaves, including the ability to prevent
 // the player from reaching to their partner.
+
+// Trigger constructor
+//
+// Sets the properties with the provided arguments for Trigger.
+function Trigger(x,y,size,health) {
+  this.x = x;
+  this.y = y;
+  this.size = size;
+  this.health = health; // The health value for trigger.
+  this.startHealth = health; // The starting health for trigger.
+  this.harmful = true; // Sets the starting state of the trigger.
+}
