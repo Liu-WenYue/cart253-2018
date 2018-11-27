76259512d844d23ecaa16db23507c61f53393c71
Liu WenYue <LiuWY0912@gmail.com>
2018-11-26 21:44:13 -0500

    P3: Added letter constructor

--- projects/project3/js/Letter.js
+++ projects/project3/js/Letter.js
@@ -2,3 +2,15 @@
 //
 // A class that defines how a letter behaves, including the ability
 // to stop the health lose of target for three rounds.
+
+// Letter constructor
+//
+// Sets the properties with the provided arguments for letter.
+function Letter(x,y,size,health) {
+  this.x = x;
+  this.y = y;
+  this.size = size;
+  this.health = health; // The health value for the letter.
+  this.startHealth = health; // The starting health for letter.
+  this.useful = true; // To check whether the letter is useful or not.
+}
