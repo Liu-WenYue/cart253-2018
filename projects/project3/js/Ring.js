6ec2273d9dd9e0d725c9c4d829b137fb1be14def
Liu WenYue <LiuWY0912@gmail.com>
2018-11-26 21:22:02 -0500

    P3: Added ring constructor

--- projects/project3/js/Ring.js
+++ projects/project3/js/Ring.js
@@ -2,3 +2,15 @@
 //
 // A class that defines how a ring behaves, including the ability
 // to transport the player to the right side of the game map.
+
+// Ring constructor
+//
+// Sets the properties with the provided arguments for ring.
+function Ring(x,y,size,health) {
+  this.x = x;
+  this.y = y;
+  this.size = size;
+  this.health = health; // The health value for the ring.
+  this.startHealth = health; // The starting health for ring.
+  this.useful = true; // To check whether the ring is useful or not.
+}
