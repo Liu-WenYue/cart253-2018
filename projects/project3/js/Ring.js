bfeb5dadf8f05772043ceb81fbefa5b03479b943
Liu WenYue <LiuWY0912@gmail.com>
2018-11-26 21:29:46 -0500

    P3: Added ring display function

--- projects/project3/js/Ring.js
+++ projects/project3/js/Ring.js
@@ -14,3 +14,15 @@ function Ring(x,y,size,health) {
   this.startHealth = health; // The starting health for ring.
   this.useful = true; // To check whether the ring is useful or not.
 }
+
+
+// display()
+//
+// Displays the ring image on the screen.
+Ring.prototype.display = function() {
+  push();
+  tint(255, this.health); // Handle the transparency of the ring.
+  // Display the ring image on screen based on its given propoerties.
+  image(ringImage,this.x,this.y,this.size,this.size);
+  pop();
+}
