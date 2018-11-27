ca3231f1ef7004204dae22ac4f281b1ab827a018
Liu WenYue <LiuWY0912@gmail.com>
2018-11-26 21:47:53 -0500

    P3: Added letter display function

--- projects/project3/js/Letter.js
+++ projects/project3/js/Letter.js
@@ -14,3 +14,15 @@ function Letter(x,y,size,health) {
   this.startHealth = health; // The starting health for letter.
   this.useful = true; // To check whether the letter is useful or not.
 }
+
+
+// display()
+//
+// Displays the letter image on the screen.
+Letter.prototype.display = function() {
+  push();
+  tint(255, this.health); // Handle the transparency of the letter.
+  // Display the letter image on screen based on its given propoerties.
+  image(letterImage,this.x,this.y,this.size,this.size);
+  pop();
+}
