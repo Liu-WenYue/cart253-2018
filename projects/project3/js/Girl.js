6c544414234d0bd0d1d33c2b047fea1e27e8d85e
Liu WenYue <LiuWY0912@gmail.com>
2018-11-26 21:13:11 -0500

    P3: Added girl constructor

--- projects/project3/js/Girl.js
+++ projects/project3/js/Girl.js
@@ -2,3 +2,15 @@
 //
 // A class that defines how a girl behaves, including the ability
 // prevent the player from reaching to their partner.
+
+// Girl constructor
+//
+// Sets the properties with the provided arguments for Girl.
+function Girl(x,y,size,health) {
+  this.x = x;
+  this.y = y;
+  this.size = size;
+  this.health = health; // The health value for girl.
+  this.startHealth = health; // The starting health for girl.
+  this.harmful = true; // Sets the starting state of the girl.
+}
